da0d7c700de88cc504570ffa2a639423341726d8
LimoHezron <kibungeihezron@gmail.com>
2025-07-03 13:40:06 +0300

    2nd day 2nd commit

--- src/components/Context/StoreContext.jsx
+++ src/components/Context/StoreContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { food_list } from "../../assets/assets";
 
 export const StoreContext = createContext(null)
+const StoreContextProvider = (props) => {
 
 const [cartItems,setCartItems] = useState ({});
 
@@ -21,7 +22,7 @@ useEffect(()=>{
     
 },[cartItems])
 
-const StoreContextProvider = (props) => {
+
 
     const contextValue = {
         food_list,
--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -10,9 +10,11 @@ const FoodDisplay = ({category}) => {
         <h2>Food dishes near you</h2>
         <div className="food-display-list">
             {food_list.map((item,index)=>{
+              if (category==="All" || category === item.category) {
                 return (
-                <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
-            )
+                  <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+              )
+              }
             })}
         </div>
     </div>
